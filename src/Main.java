88cbd40e56176788490b20f420f1fb4f96329430
Himanshu-Goyal21 <goyalhimanshu.hgs@gmail.com>
2026-01-18 22:12:54 +0530

    Refine comments for reading and printing integers
    Updated comments for clarity and removed TODOs.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // TODO: Read two integers
+        // Read two integers
         System.out.print("Enter Your First Number");
          int a1 = scanner.nextInt();
         System.out.print("Enter Your Second Number");
          int a2 = scanner.nextInt();
-
-        // TODO: Print their sum  using println
+        //  Print their sum  using println
         int sum = a1 + a2 ;
         System.out.print(+ sum);
         scanner.close();
