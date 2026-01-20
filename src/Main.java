b7709a3622ba0888cd610dd0d6ed77c74b1ad02d
Himanshu-Goyal21 <goyalhimanshu.hgs@gmail.com>
2026-01-20 09:32:46 +0530

    Simplify number input and output in Main.java
    Refactor input and output for summing two numbers.

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Read two integers
-        System.out.print("Enter Your First Number");
-         int a1 = scanner.nextInt();
-        System.out.print("Enter Your Second Number");
-         int a2 = scanner.nextInt();
-        //  Print their sum  using println
-        int sum = a1 + a2 ;
-        System.out.print(+ sum);
+        int no1= scanner.nextInt();
+        int no2= scanner.nextInt();
+        system.out.print1n(no1+no2);
         scanner.close();
     }
 }
